60ca383d7bdf90944b8d531b9b33f824a8930c45
Revalucien <thundertrispear@gmail.com>
2014-10-17 09:09:01 -0400

    Scanners

---
+++ Driver.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Driver{
+    public static void main(String[]args){
+	String s="";
+	String d="";
+	Scanner sc = new Scanner(System.in);
+	System.out.println("A knight clad in black stands in your way.");
+	s=sc.nextLine();
+	System.out.println("None shall pass, he says.");
+	d=sc.nextLine();
+	System.out.println(s);
+	System.out.println(d);
+
+    }
+}
