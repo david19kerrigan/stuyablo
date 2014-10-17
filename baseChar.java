f12c60024b9709448f9fe907be3f4340071a5b45
Javis Wu <wujavis@gmail.com>
2014-10-17 09:25:07 -0400

    9:25 am

--- baseChar.java
+++ baseChar.java
@@ -7,13 +7,16 @@ public class baseChar{
     private int dexterity=5;
     private int mana=0;
     public void attacking(baseChar other){
+	//consider this basechar's attack and dexterity and the other basechar's
+	// defense. range of attack damage is +-2. dexterity determines chance
+	// of hit. defense negates some damage.
 	Random rand = new Random();
 	int n = rand.nextInt(attack+2)+attack-2;
 	System.out.println(n);
     }
 }
 
-//number 2
+
 	
 	
 	
