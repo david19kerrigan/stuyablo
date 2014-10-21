cacd5c5f724c88d5e761fb7a206e9e34e279fb81
david.kerrigan <david19kerrigan@gmail.com>
2014-10-21 09:10:23 -0400

    42

---
+++ Mage.java
@@ -0,0 +1,8 @@
+public class Warrior extends baseChar{
+    //private String name = "DEFAULT";
+    private int attack=10;
+    private int defense=2;
+    public Mage(String name){
+    	super(name);
+    }
+}
---
+++ Player.java
@@ -0,0 +1,4 @@
+public class Warrior extends baseChar{
+    private int attack=10;
+    private int defense=7;
+}
