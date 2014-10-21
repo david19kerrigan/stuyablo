9cfb72b3aea61ac886f9a7fc962510f8eff48909
david.kerrigan <david19kerrigan@gmail.com>
2014-10-21 09:24:50 -0400

    dfsdfsdf

--- Scanner.java
+++ Scanner.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Driver{
+public class Scanner{
     public static void main(String[]args){
 	String job="";
 	String s="";
@@ -16,11 +16,12 @@ public class Driver{
 	    }
 	if (job.equals("Mage")){
 	    System.out.println("You are a Mage.");
-	    //   Mage player = new Mage();
+	    Mage player = new Mage();
 	}
 	//Assuming the player does not type in an invalid class.
 	//Case sensitive
 	System.out.println("A knight clad in black stands in your way.");
+	Warrior knight = new Warrior("bad dude");
 	try {
 	    Thread.sleep(1000);
 	}catch(Exception e){}
@@ -32,8 +33,13 @@ public class Driver{
 	s=sc.nextLine();
 	
 	if (s.equals("fight")){
+	    while(knight.getHealth>0){
 	    System.out.println("What do you want to do?");
 		//list moves here, method of class
+	    
+	        
+		s = sc.nextLine();
+
 	}
 	if (s.equals("run")){
 	    System.out.println("You run away with your tail between your legs.");
